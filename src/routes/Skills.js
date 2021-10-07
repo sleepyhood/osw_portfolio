6d3a778c8847bf0c896d5cd098640c4d34100423
sleepyhood <smartosw@korea.ac.kr>
2021-10-07 09:06:25 +0900

    20211007 icon update

--- src/routes/Skills.js
+++ src/routes/Skills.js
@@ -1,6 +1,23 @@
 import React, { useState, useEffect } from "react";
+import c from "../imgs/icons/c.svg";
+import cplusplus from "../imgs/icons/cplusplus.svg";
+import css from "../imgs/icons/css3.svg";
+import html from "../imgs/icons/html-5.svg";
+import js from "../imgs/icons/javascript.svg";
+import nodeJs from "../imgs/icons/node-js.svg";
+import php from "../imgs/icons/php.svg";
+import python from "../imgs/icons/python.svg";
+import react from "../imgs/icons/react.svg";
 
 const Skills = () => {
+  const iconWH = "70px";
+  const iconStyle = {
+    borderRadius: "0",
+    width: iconWH,
+    height: iconWH,
+    backgroundColor: "transparent",
+    boxShadow: "none",
+  };
   return (
     <div className="skills">
       <div className="container skills" style={{ backgroundColor: "#f8f8f8" }}>
@@ -13,7 +30,18 @@ const Skills = () => {
             <div className="introduce skills">
               <ul>
                 <li>
-                  <div>Front End</div>
+                  <div>
+                    Front End
+                    <img src={html} style={iconStyle} />
+                    <img src={css} style={iconStyle} />
+                    <img src={js} style={iconStyle} />
+                    <img src={react} style={iconStyle} />
+                    <img src={php} style={iconStyle} />
+                    <img src={nodeJs} style={iconStyle} />
+                    <img src={c} style={iconStyle} />
+                    <img src={cplusplus} style={iconStyle} />
+                    <img src={python} style={iconStyle} />
+                  </div>
                 </li>
                 <li>Back End</li>
                 <li>Algorithm</li>
